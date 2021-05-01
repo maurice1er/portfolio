34cf455ca24eb5f092a01b04782e201ea2001ec9
Maurice1er <armeldrey@gmail.com>
2021-05-01 03:22:58 +0000

    first commit:portfolio

---
+++ react/src/components/portfolio.jsx
@@ -0,0 +1,250 @@
+import React from "react";
+
+//import stock
+import stock from "../img/image1.jpg";
+import stock1 from "../img/image2.jpg";
+import stock2 from "../img/image3.jpg";
+import stock3 from "../img/image4.jpg";
+import stock4 from "../img/image5.jpg";
+import stock5 from "../img/image6.jpg";
+
+class Portfolio extends React.Component {
+  render() {
+    return (
+      <section id="work" className="portfolio-mf sect-pt4 route">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-12">
+              <div className="title-box text-center">
+                <h3 className="title-a">Portfolio</h3>
+                <p className="subtitle-a">
+                  Here are some projects of all kinds done.
+                </p>
+                <div className="line-mf"></div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={stock2} data-lightbox="gallery-vmarine">
+                  <div className="work-img">
+                    <img src={stock2} alt="hello" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Marathon de l'Info</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Php, Javascript, MySQL
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={stock1}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock2}
+                  data-lightbox="gallery-vmarine"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={stock3} data-lightbox="gallery-aguadeluz">
+                  <div className="work-img">
+                    <img src={stock3} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Automated Data Cleaning</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            ReactJS, Flask, Sklearn, Firebase
+                          </span>{" "}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={stock3}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock4}
+                  data-lightbox="gallery-aguadeluz"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={stock5} data-lightbox="gallery-todo">
+                  <div className="work-img">
+                    <img src={stock5} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Data Integration</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Python, Pentaho, Elasticsearch, Logstash, Cassandra
+                          </span>
+                          {/*} / <span className="w-date">18 Sep. 2018</span>*/}
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={stock2}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock3}
+                  data-lightbox="gallery-todo"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="work-box">
+                <a href={stock4} data-lightbox="gallery-medlingos">
+                  <div className="work-img">
+                    <img src={stock4} alt="" className="img-fluid" />
+                  </div>
+                  <div className="work-content">
+                    <div className="row">
+                      <div className="col-sm-8">
+                        <h2 className="w-title">Data Scraping</h2>
+                        <div className="w-more">
+                          <span className="w-ctegory">
+                            Python, Pandas, BeautifulSoup
+                          </span>
+                        </div>
+                      </div>
+                      <div className="col-sm-4">
+                        <div className="w-like">
+                          <span className="ion-ios-plus-outline"></span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+                <a
+                  href={stock1}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock3}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+                <a
+                  href={stock5}
+                  data-lightbox="gallery-medlingos"
+                  style={{ display: "none" }}
+                >
+                  jsx-a11y/anchor-has-content warning
+                </a>
+              </div>
+            </div>
+            <div className="col-md-4">
+                <div className="work-box">
+                  <a href={stock1} data-lightbox="gallery-medlingos">
+                    <div className="work-img">
+                      <img src={stock1} alt="" className="img-fluid" />
+                    </div>
+                    <div className="work-content">
+                      <div className="row">
+                        <div className="col-sm-8">
+                          <h2 className="w-title">Image Classification</h2>
+                          <div className="w-more">
+                            <span className="w-ctegory">
+                              Python, Pandas, CV2, PyTorch, Flask
+                            </span>
+                          </div>
+                        </div>
+                        <div className="col-sm-4">
+                          <div className="w-like">
+                            <span className="ion-ios-plus-outline"></span>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </a>
+                  <a
+                    href={stock1}
+                    data-lightbox="gallery-medlingos"
+                    style={{ display: "none" }}
+                  >
+                    jsx-a11y/anchor-has-content warning
+                  </a>
+                  <a
+                    href={stock3}
+                    data-lightbox="gallery-medlingos"
+                    style={{ display: "none" }}
+                  >
+                    jsx-a11y/anchor-has-content warning
+                  </a>
+                  <a
+                    href={stock5}
+                    data-lightbox="gallery-medlingos"
+                    style={{ display: "none" }}
+                  >
+                    jsx-a11y/anchor-has-content warning
+                  </a>
+                </div>
+              </div>
+            </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
